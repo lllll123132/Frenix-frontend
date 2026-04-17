378cb1ed34dcb0991165a695a38680e79101f6e9
Hiren <ghg64272@gmail.com>
2026-04-17 16:16:09 +0530

    fix: disabled static export to support dynamic API routes

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  /* 
-     Note: 'rewrites' are removed for static export. 
-     You should point your API calls directly to https://api.frenix.sh
-  */
+  /* config options here */
 };
 
 export default nextConfig;
